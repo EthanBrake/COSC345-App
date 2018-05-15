930c2d1a7d71afc523b954d27bc597e8c6f0b8c2
EthanBrake <38169596+EthanBrake@users.noreply.github.com>
2018-05-15 18:51:09 +1200

    Test1

---
+++ Xcode/COSC345/SecondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.swift
+//  COSC345
+//
+//  Created by Ethan Brake on 3/23/18.
+//  Copyright © 2018 Ethan Brake. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         self.title = "Second"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
