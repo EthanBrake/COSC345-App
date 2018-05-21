5b6e075f7fa181f420ed5fc75f09d02c4c9cb768
Robert Giles <rgiles@oucs1392.otago.ac.nz>
2018-05-21 15:47:59 +1200

    Robert's Changes

--- Xcode/COSC345/SecondViewController.swift
+++ Xcode/COSC345/SecondViewController.swift
@@ -8,8 +8,46 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var list: [String] = []
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            self.list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+
+    @IBOutlet weak var input: UITextField!
+    
+    @IBAction func additem(_ sender: Any) {
+        if(input.text != "")
+        {
+            list.append(input.text!)
+            myTableView.reloadData()
+            input.text = ""
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
