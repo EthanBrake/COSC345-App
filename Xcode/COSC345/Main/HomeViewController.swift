40bb454911a2ca289dd33b62724bff3fa3ba38d2
Ethan Brake <braet759@student.otago.ac.nz>
2018-09-26 15:55:22 +1200

    Fixed main menu constraints

--- Xcode/COSC345/Main/HomeViewController.swift
+++ Xcode/COSC345/Main/HomeViewController.swift
@@ -17,28 +17,5 @@ class HomeViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    @IBAction func clickDiaryVC(_ sender: AnyObject) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let secondVC = storyboard.instantiateViewController(withIdentifier: "DiaryViewController")as!DiaryViewController
-        self.navigationController?.pushViewController(secondVC, animated: true)
-    }
-    
-    @IBAction func clickGoalsVC(_ sender: AnyObject) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let thirdVC = storyboard.instantiateViewController(withIdentifier: "GoalsViewController")as!GoalsViewController
-        self.navigationController?.pushViewController(thirdVC, animated: true)
-    }
-    
-    @IBAction func clickBreathingVC(_ sender: AnyObject) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let forthVC = storyboard.instantiateViewController(withIdentifier: "BreathingViewController")as!BreathingViewController
-        self.navigationController?.pushViewController(forthVC, animated: true)
-    }
-    
-    @IBAction func clickInformationVC(_ sender: AnyObject) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let fifthVC = storyboard.instantiateViewController(withIdentifier: "InformationViewController")as!InformationViewController
-        self.navigationController?.pushViewController(fifthVC, animated: true)
-    }
+
 }
