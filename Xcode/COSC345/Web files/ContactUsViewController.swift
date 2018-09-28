0c6e0a5e5280b564ccc005b7fdd80d09c5ded1c1
Charybdiss <ktmlaing@gmail.com>
2018-09-28 15:16:01 +1200

    added some usage info to the contact page

--- Xcode/COSC345/Web files/ContactUsViewController.swift
+++ Xcode/COSC345/Web files/ContactUsViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 class ContactUsViewController: UIViewController {
 
-     override func viewDidLoad() {
+    @IBAction func usage(_ sender: Any) {
+        
+        // create the alert
+        let alert = UIAlertController(title: "App Usage:", message: "Diary - Keep track of your thoughts.\n\nGoals - Set goals for yourself to try and achieve (eg. 'Talk to a stranger').\n\nRelaxing Image - Choose a relaxing image to zone out / try relax to.\n\nBreathing Exercise - An animation made to do breathing exercises to. More information can be found in the breathing exercise section.\n\nContact Us - (the page you are on). This page has the app use info and a link to our Facebook group.\n\nInformation - A collection of weblinks to helpful information about Social Anxiety.", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
